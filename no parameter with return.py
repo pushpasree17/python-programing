e15d2d6d5236ace641b9ff6d4fe456a136f6b2bb
pushpasree17 <sree06262@gmail.com>
2025-09-10 18:01:31 +0530

    Add files via upload

---
+++ no parameter with return.py
@@ -0,0 +1,7 @@
+def add():
+    x=int(input("enter the x"))
+    y=int(input("enter the y"))
+    z=int(input("enter the y"))
+    z=x+y
+    return(z)
+print("the addiction of two numbers is",add())
